77638d7369586ce7b1695e439d201f952a896ce0
DangHoangLong <long.711.sg@gmail.com>
2026-04-26 22:21:46 +0700

    update

--- main.ts
+++ main.ts
@@ -4,5 +4,5 @@ basic.clearScreen()
 basic.forever(function () {
     basic.showString("0x27(39)")
     lcd.displayText("MakerEDU Product", 1, 1)
-    lcd.displayText("LCD1602/LCD2004", 1, 1)
+    lcd.displayText("LCD1602/LCD2004", 1, 2)
 })
