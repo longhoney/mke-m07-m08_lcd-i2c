8993ff2f822b8785e1857eb917537e3a8f75e3ca
DangHoangLong <long.711.sg@gmail.com>
2026-04-27 09:33:03 +0700

    show string lam cham hien thi len LCD

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.showIcon(IconNames.Sad)
 basic.pause(500)
 basic.clearScreen()
 basic.forever(function () {
-    basic.showString("0x27(39)")
     lcd.displayText("MakerEDU Product", 1, 1)
     lcd.displayText("LCD1602/LCD2004", 1, 2)
+    basic.showString("0x27(39)")
 })
