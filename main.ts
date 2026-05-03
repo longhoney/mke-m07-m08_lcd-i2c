86268367d1f7366baec2ea0c7be7f5882e4fc7cc
DangHoangLong <long.711.sg@gmail.com>
2026-05-03 09:31:07 +0700

    update

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ basic.forever(function () {
     lcd.displayText("MakerEDU Product", 1, 1)
     lcd.displayText("LCD1602/LCD2004", 1, 2)
     lcd.displayText("Address: 0x27", 1, 3)
-    lcd.displayText("Dec: 39", 1, 3)
+    lcd.displayText("Dec: 39", 1, 4)
 })
