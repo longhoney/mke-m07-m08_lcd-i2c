918986b240ee801ea199802ff407082e9b1aaaa7
DangHoangLong <long.711.sg@gmail.com>
2026-05-03 09:30:37 +0700

    fix for M07 & M08

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
-basic.showIcon(IconNames.Sad)
-basic.pause(500)
-basic.clearScreen()
 basic.forever(function () {
     lcd.displayText("MakerEDU Product", 1, 1)
     lcd.displayText("LCD1602/LCD2004", 1, 2)
-    basic.showString("0x27(39)")
+    lcd.displayText("Address: 0x27", 1, 3)
+    lcd.displayText("Dec: 39", 1, 3)
 })
